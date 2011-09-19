558cc06efee7d886fe54345f30fd1b27259b48e9
Bartosz Pieńkowski <pienkowb@gmail.com>
2011-09-19 02:37:21 +0200

    Poprawiono komentarze w pliku serial.h.

--- serial.h
+++ serial.h
@@ -1,15 +1,15 @@
 /**
- * Reads value assigned to specified user in configuration file
- * @param user Username
- * @param buffer Output buffer for serial number
- * @param size Buffer size
- * @return 0 on success or -1 on failure
+ * Reads a serial number assigned to the user from the configuration file
+ * @param user   User name
+ * @param buffer Output buffer for a serial number
+ * @param size   Buffer size
+ * @return       0 on success, -1 on failure
  */
 int get_serial(const char* user, char* buffer, unsigned int size);
 
 /**
- * Checks whether a USB device with specified serial number is connected
+ * Checks whether a USB device with the specified serial number is connected
  * @param serial Serial number
- * @return Check result
+ * @return       1 if connected, otherwise 0
  */
 int check_serial(const char* serial);
